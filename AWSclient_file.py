b97e1423d2577e05e2501febb84c6a7674b6b320
Ally Ryan <allyryan422@gmail.com>
2024-04-30 14:01:11 -0500

    test client with the AWS URL

---
+++ AWSclient_file.py
@@ -0,0 +1,73 @@
+import requests
+import json
+
+payload = { "shapeID": "Shape101", "sideDimension" : "7"}
+header = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+r = requests.post("https://2xfhk9btb9.us-east-2.awsapprunner.com/updateDB", json=payload, headers=header)
+data = r.json()
+print("Insert data for Shape101: ")
+print(data["status"])
+
+payload = { "shapeID": "Shape102", "sideDimension" : "24"}
+header = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+r = requests.post("https://2xfhk9btb9.us-east-2.awsapprunner.com/updateDB", json=payload, headers=header)
+data = r.json()
+print("Insert data for Shape102: ")
+print(data["status"])
+
+payload = { "shapeID": "Shape103", "sideDimension" : "10"}
+header = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+r = requests.post("https://2xfhk9btb9.us-east-2.awsapprunner.com/updateDB", json=payload, headers=header)
+data = r.json()
+print("Insert data for Shape103: ")
+print(data["status"])
+
+payload = { "shapeID": "Shape104", "sideDimension" : "90"}
+header = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+r = requests.post("https://2xfhk9btb9.us-east-2.awsapprunner.com/updateDB", json=payload, headers=header)
+data = r.json()
+print("Insert data for Shape104: ")
+print(data["status"])
+
+payload = { "shapeID": "Shape105", "sideDimension" : "15"}
+header = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+r = requests.post("https://2xfhk9btb9.us-east-2.awsapprunner.com/updateDB", json=payload, headers=header)
+data = r.json()
+print("Insert data for Shape105: ")
+print(data["status"])
+
+payload = {}
+header = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+r = requests.post("https://2xfhk9btb9.us-east-2.awsapprunner.com/updateDB", json=payload, headers=header)
+data = r.json()
+print("Insert empty data attempt: ")
+print(data["status"])
+
+payload = {"wantedVal": "ShapeVolume"}
+header = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+r = requests.post("https://2xfhk9btb9.us-east-2.awsapprunner.com/queryData", json=payload, headers=header)
+data = r.json()
+print("Query results for shapeVolume: ")
+print(data)
+
+payload = {"wantedVal": "ShapeVolume", "condition": "ShapeID", "conditionVal":"Shape103", "operator": "="}
+header = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+r = requests.post("https://2xfhk9btb9.us-east-2.awsapprunner.com/queryData", json=payload, headers=header)
+data = r.json()
+print("Query results for: ShapeVolume WHERE ShapeID = Shape103 ")
+print(data)
+
+payload = {"shapeID": "Shape102", "sideDimension" : "30"}
+header = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+r = requests.post("https://2xfhk9btb9.us-east-2.awsapprunner.com/updateShape", json=payload, headers=header)
+data = r.json()
+print("Update volume for Shape102: ")
+print(data["Attributes"]["ShapeVolume"])
+print(data["status"])
+
+
+
+
+
+
+
